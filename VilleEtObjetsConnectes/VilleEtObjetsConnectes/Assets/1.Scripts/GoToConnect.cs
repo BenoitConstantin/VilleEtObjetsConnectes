7987642e87061d792ceb3013fdedff6edd25aa19
CHERFALLOT Christophe <cherfallot.christophe@formation.enjmin.fr>
2016-10-24 16:57:18 +0200

    Ecran touchToplay + passage a écran de connexion

---
+++ VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/GoToConnect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToConnect : MonoBehaviour {
+    
+	public void LaunchConnection()
+    {
+        GameManager.Instance.LaunchConnect();
+    } 
+}
