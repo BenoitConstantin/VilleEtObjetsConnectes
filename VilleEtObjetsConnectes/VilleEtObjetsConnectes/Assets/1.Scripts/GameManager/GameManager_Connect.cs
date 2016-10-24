6e67e647e8e9609d3a6dc67af151bf324cc77907
CONSTANTIN <benoitc@equilibregames.com>
2016-10-24 16:50:50 +0200

    -ADD : GameManager coord update

--- VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/GameManager/GameManager_Connect.cs
+++ VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/GameManager/GameManager_Connect.cs
@@ -8,6 +8,11 @@ public class GameManager_Connect : GameManagerState
     [SerializeField]
     string connectSceneName = "Connect";
 
+    [SerializeField]
+    float timeBetween2Update = 0.5f;
+
+    float timer = -1;
+
     public override void OnActivation(string previousState, string info = "")
     {
         base.OnActivation(previousState, info);
@@ -17,7 +22,11 @@ public class GameManager_Connect : GameManagerState
 
     void Update()
     {
-        gameManager.GetPlayers();
+        if (Time.time > timer)
+        {
+            gameManager.GetPlayers();
+            timer = Time.time + timeBetween2Update;
+        }
     }
 
 }
