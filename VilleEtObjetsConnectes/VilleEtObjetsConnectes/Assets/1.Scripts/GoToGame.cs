c99dab6c731ac13a8bd08155508f74f274c6c015
CHERFALLOT Christophe <cherfallot.christophe@formation.enjmin.fr>
2016-10-25 17:17:15 +0200

    Intégration travail des graphes sur les menus

---
+++ VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/GoToGame.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GoToGame : MonoBehaviour {
+
+	public void LaunchGame()
+    {
+        GameManager.Instance.LaunchGame();
+    }
+}
