c38159ff5ca2cbe4548ca6d5493baee338dc4013
CONSTANTIN <benoitc@equilibregames.com>
2016-10-28 14:26:29 +0200

    -ADD : End game

---
+++ VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/GameManager/GameManager_End.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager_End : GameManagerState {
+
+    [SerializeField]
+    string gameSceneName = "Game";
+
+
+    public override void OnDesactivation(string nextState, string info = "")
+    {
+        base.OnDesactivation(nextState, info);
+        SceneManager.UnloadScene(gameSceneName);
+    }
+
+}
