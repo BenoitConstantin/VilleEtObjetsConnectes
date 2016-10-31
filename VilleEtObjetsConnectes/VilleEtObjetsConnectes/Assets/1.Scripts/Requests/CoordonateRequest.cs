cf092a5883dc14bb6032abb1a713f696f7f5d228
thalesalexcin@gmail.com <thalesalexcin@gmail.com>
2016-10-31 16:51:31 +0100

    - fixing gps

--- VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/Requests/CoordonateRequest.cs
+++ VilleEtObjetsConnectes/VilleEtObjetsConnectes/Assets/1.Scripts/Requests/CoordonateRequest.cs
@@ -25,29 +25,26 @@ public class CoordonateRequest : MonoBehaviour {
     float timer = -1;
     long lastUpdate = -1;
 
-
-    Vector2 translation;
-    Vector2 columnTransformationMatrix1;
-    Vector2 columnTransformationMatrix2;
+    private float _Width;
+    private float _Height;
+    private float _Shear_amount;
 
     void Awake()
     {
-        //Fing the matrix transformation that send GPS coordonate to the ortho-normalized base
-        translation = -borderLeftTopGPS;
-
-
-        //from ortho-normalized to GPS
-        columnTransformationMatrix1 = borderLeftTopGPS - borderLeftBotGPS;
-        columnTransformationMatrix2 = borderRightBotGPS - borderLeftBotGPS;
-
-        //from GPD to ortho-normalized
-        float det = (columnTransformationMatrix1.x * columnTransformationMatrix2.y - columnTransformationMatrix1.y * columnTransformationMatrix2.x);
-
-        columnTransformationMatrix1 = 1f / det * new Vector2(columnTransformationMatrix2.y, -columnTransformationMatrix1.y);
-        columnTransformationMatrix2 = 1f / det * new Vector2(-columnTransformationMatrix2.x, columnTransformationMatrix1.x);
+        _Width = borderRightTopGPS.x - borderLeftTopGPS.x;
+        _Height = borderRightTopGPS.y - borderRightBotGPS.y;
+        _Shear_amount = borderLeftTopGPS.x - borderLeftBotGPS.x;
     }
 
-
+    // Returns a vector normalized. If any value is not between 0 and 1, the point isn't on the area.
+    private Vector2 _GetNormalizedCoordinates(Vector2 p)
+    {
+        Vector2 pt_offset = p - borderLeftTopGPS;
+        Vector2 scaled = new Vector2();
+        scaled.y = -pt_offset.y / _Height;
+        scaled.x = (pt_offset.x + _Shear_amount * scaled.y) / _Width;
+        return scaled;
+    }
 
     public void StartRequest()
     {
@@ -70,7 +67,6 @@ public class CoordonateRequest : MonoBehaviour {
             timer = Time.time + timeBetween2Update;
         }
     }
-
 
     void RequestCoordonates()
     {
@@ -103,8 +99,14 @@ public class CoordonateRequest : MonoBehaviour {
             {
                 if(p.Id == JNode[i]["id"].AsInt)
                 {
+                    var normalizedVector = _GetNormalizedCoordinates(new Vector2(JNode[i]["x"].AsFloat, JNode[i]["y"].AsFloat));
+
+                    normalizedVector.x = (normalizedVector.x - 0.5f) * 18;
+                    normalizedVector.y = (normalizedVector.y - 0.5f) * 18;
+                    normalizedVector.y *= -1;
+
                     //Apply transformation matrix to GPS coordonate
-                    p.MoveTo(new Vector2((JNode[i]["x"].AsFloat* columnTransformationMatrix1.x + JNode[i]["y"].AsFloat* columnTransformationMatrix2.x), (JNode[i]["x"].AsFloat * columnTransformationMatrix1.y + JNode[i]["y"].AsFloat * columnTransformationMatrix2.y)) + translation, deltaTime/1000);
+                    p.MoveTo(normalizedVector, deltaTime/1000);
                 }
             }
         } 
